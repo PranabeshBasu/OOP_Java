7958f14f4f8e44d4dec7d56c8beb28524a8ec503
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-04-13 20:14:50 +0530

    Multiple Inheritence via Interfaces

--- InterfaceImplementation.java
+++ InterfaceImplementation.java
@@ -1,4 +1,5 @@
 import java.util.*;
+//Interface 1
 interface Car {
     static final int count = 0; // That's how we define variables in interface...these are not instance variable
     public void start();
@@ -10,9 +11,13 @@ interface Car {
         System.out.println("This car has high mileage!!");
     }
 }
-class Manual implements Car {
+//Interface 2
+interface sunRoof {
+    public void whichSunRoof();
+}
+class Manual implements Car, sunRoof {
     public void start() {
-        System.out.println("The car is starting...."+count);
+        System.out.println("The car is starting....");
     }
     public void noise() {
         System.out.println("The car is making noise!!");
@@ -21,16 +26,14 @@ class Manual implements Car {
     public void isthereAirBags() {
         System.out.println("There are 4 air bags");
     }
+    public void whichSunRoof() {
+        System.out.println("The manual car has a normal sunroof");
+    }
 }
 public class InterfaceImplementation {
     public static void main(String[] args) {
         Manual m1 = new Manual();
         m1.start();
-        Manual m2 = new Manual();
-        m2.start();
-        // m1.start();
-        // m1.noise();
-        // m1.isthereAirBags();
-        // Car.mileage();
+        m1.whichSunRoof();
     }
 }
