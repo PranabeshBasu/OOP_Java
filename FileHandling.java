4f651c6c50beccfc8a08392f1d2d3967a16f3513
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-06-03 15:47:42 +0530

    File Handling - OOP

---
+++ FileHandling.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+public class FileHandling {
+    public static void main(String[] args) {
+        File file = new File("PATH/texting.txt");
+        System.out.println(file.exists());
+        try {
+            File newfile = new File("PATH/text.txt");
+            if (newfile.createNewFile()) {
+                System.out.println("File created successfully"+newfile.getName());
+            }
+            else {
+                System.out.println("file already exists");
+            }
+        }
+        catch(IOException e) {
+            System.out.println("An error occured!!");
+            e.printStackTrace();
+        }
+        System.out.println(file.exists());
+        System.out.println(file.canRead());
+    }
+}
